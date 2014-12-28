564115f3bc7c4ad086c17eb05c70e8ae96a2b337
Brahim Boukobba <brahim@boukobba.com>
2014-12-28 21:06:33 +0100

    le cochon c'est bon

---
+++ view/role/ajout-role.php
@@ -0,0 +1,16 @@
+
+<?php
+if(isset($_SESSION["error"])){
+    echo "<div class='alert alert-danger'>".$_SESSION['error']."</div>";  
+    unset($_SESSION["error"]);
+}
+  
+
+$form= new Form("index.php?action=ajout-roleType","post");
+echo $form->openForm();
+echo $form->text("label","Label:",array_key_exists("label", $_POST)?$_POST["name"]:null);
+echo $form->number("ordre","Ordre:",array_key_exists("ordre", $_POST)?$_POST["firstname"]:null);
+echo $form->submit();
+echo $form->closeForm();
+?>
+</div>
